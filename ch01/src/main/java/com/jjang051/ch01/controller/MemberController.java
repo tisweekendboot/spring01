14b974fb712fac746fca8e6b50459280de8cae8d
tisweekendboot <tisweekendboot@gmail.com>
2023-03-25 16:03:40 +0900

    resuest test

--- ch01/src/main/java/com/jjang051/ch01/controller/MemberController.java
+++ ch01/src/main/java/com/jjang051/ch01/controller/MemberController.java
@@ -3,7 +3,9 @@ package com.jjang051.ch01.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
@@ -11,6 +13,16 @@ public class MemberController {
   @GetMapping("/member/login")
   public String login() {
     return "/member/login";
+  }
+
+  @PutMapping("/member/login")
+  @ResponseBody
+  public String loginPut(String userId, String userPw) {
+    if (userId.equals("jjang051") && userPw.equals("1234")) {
+      return "login success";
+    } else {
+      return "login fail";
+    }
   }
 
   @PostMapping("/member/login")
