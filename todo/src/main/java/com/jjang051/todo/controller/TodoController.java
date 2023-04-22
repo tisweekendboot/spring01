942417d19f76079cc10c4423bb80db9cd4dabafd
tisweekendboot <tisweekendboot@gmail.com>
2023-04-22 16:46:23 +0900

    add

--- todo/src/main/java/com/jjang051/todo/controller/TodoController.java
+++ todo/src/main/java/com/jjang051/todo/controller/TodoController.java
@@ -3,7 +3,9 @@ package com.jjang051.todo.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @Controller
@@ -13,5 +15,11 @@ public class TodoController {
   @GetMapping("/index")
   public String home() {
     return "/todo/index";
+  }
+
+  @PostMapping("/add")
+  @ResponseBody
+  public String add() {
+    return "잘받았어요.";
   }
 }
